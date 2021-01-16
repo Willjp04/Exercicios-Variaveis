68eeab3a9548cfe11908f7ac2b5477ff64ead21e
Willian Sostenes <sostenes655@gmail.com>
2021-01-16 02:09:12 -0300

    Realizando exercicios de variaveis primitivas

--- ExerciciosVariaveis/src/exerciciosvariaveis/ExerciciosVariaveis.java
+++ ExerciciosVariaveis/src/exerciciosvariaveis/ExerciciosVariaveis.java
@@ -5,14 +5,24 @@ package exerciciosvariaveis;
 public class ExerciciosVariaveis {
     
     public static void main(String[] args) {
-        int idadeWillian = 28;
+     /*   int idadeWillian = 28;
         int idadeJoyce = 25;
         int idadeHelloisa = 6;
         System.out.println("A idade do Willian é " + idadeWillian + " Anos");
         System.out.println("A idade da Joyce é " + idadeJoyce + " Anos");
         System.out.println("A idade da Helloisa é " + idadeHelloisa + " Anos");
         
-       
+        */
+       String nome = "Willian";
+       String endereco = "Rua Luis fiqueira 101";
+       String telefone = "11988216426";
+        System.out.println("O " + nome + " domicíliado no endereço " + endereco + " e telefone " 
+                + telefone + " não possui nenhum tipo de pendecia");
+        
+     
+     
+     
+     
     }
     
 }
