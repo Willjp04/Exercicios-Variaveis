c154ff7b3e3017cfe25361a8580ef349c00c4ea9
Willian Sostenes <sostenes655@gmail.com>
2021-01-16 01:55:47 -0300

    Iniciando exercicios de variaveis

---
+++ ExerciciosVariaveis/src/exerciciosvariaveis/ExerciciosVariaveis.java
@@ -0,0 +1,18 @@
+
+package exerciciosvariaveis;
+
+
+public class ExerciciosVariaveis {
+    
+    public static void main(String[] args) {
+        int idadeWillian = 28;
+        int idadeJoyce = 25;
+        int idadeHelloisa = 6;
+        System.out.println("A idade do Willian é " + idadeWillian + " Anos");
+        System.out.println("A idade da Joyce é " + idadeJoyce + " Anos");
+        System.out.println("A idade da Helloisa é " + idadeHelloisa + " Anos");
+        
+       
+    }
+    
+}
