5bc5468e4a192a6bf1e5e98b34369da73639bde3
Willian Sostenes <sostenes655@gmail.com>
2021-01-16 03:24:55 -0300

    Treinando com operadores de comparação

--- ExerciciosVariaveis/src/exerciciosvariaveis/Operadores.java
+++ ExerciciosVariaveis/src/exerciciosvariaveis/Operadores.java
@@ -1,20 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exerciciosvariaveis;
 
-/**
- *
- * @author Forceone
- */
+
 public class Operadores {
     public static void main(String[] args) {
         float numero1 = 10;
         float numero2 = 20;
         float divisao = numero1/numero2;
+        double resto = 29%2;
         System.out.println("A divisão é " + divisao);
+        System.out.println("O resto da divisão é " + resto);
     };
     
 }
---
+++ ExerciciosVariaveis/src/exerciciosvariaveis/OperadoresDeComparacao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosvariaveis;
+
+/**
+ *
+ * @author Forceone
+ */
+public class OperadoresDeComparacao {
+    public static void main(String[] args) {
+       boolean a1 = 10 != 20;
+       boolean a2 = 20 <= 30;
+        System.out.println(a1);
+        System.out.println(a2);
+       
+    }
+}
