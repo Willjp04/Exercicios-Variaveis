fdecd8da3adefdadbc2de133a5c9e29ff8b3839e
Willian Sostenes <sostenes655@gmail.com>
2021-01-16 02:26:03 -0300

    Continuação dos exercicios

--- ExerciciosVariaveis/src/exerciciosvariaveis/RelatorioDePendencia.java
+++ ExerciciosVariaveis/src/exerciciosvariaveis/RelatorioDePendencia.java
@@ -1,13 +1,12 @@
-
 package exerciciosvariaveis;
 
-
 public class RelatorioDePendencia {
-     public static void main(String[] args) {
-     String nome = "Willian";
-       String endereco = "Rua Luis fiqueira 101";
-       String telefone = "11988216426";
-        System.out.println("O " + nome + " domicíliado no endereço " + endereco + " e telefone " 
+
+    public static void main(String[] args) {
+        String nome = "Willian";
+        String endereco = "Rua Luis fiqueira 101";
+        String telefone = "11988216426";
+        System.out.println("O " + nome + " domicíliado no endereço " + endereco + " e telefone "
                 + telefone + " não possui nenhum tipo de pendecia");
-}
+    }
 };
