9b729856b232cea223032f45df788c64781c2f7c
Willian Sostenes <sostenes655@gmail.com>
2021-01-17 03:43:24 -0300

    Realizando testes com While

---
+++ ExerciciosVariaveis/src/exerciciosvariaveis/ControleDeFluxo4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosvariaveis;
+
+/**
+ *
+ * @author Forceone
+ */
+public class ControleDeFluxo4 {
+
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador < 10) {
+            System.out.println(contador);
+            contador++;
+           // contador--;
+        }
+        
+
+    }
+}
