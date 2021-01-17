2c20c356da4c5400b9eaf3f00c7772e547db754f
Willian Sostenes <sostenes655@gmail.com>
2021-01-17 03:52:06 -0300

    Utilizando o DO WHILE

--- ExerciciosVariaveis/src/exerciciosvariaveis/ControleDeFluxo4.java
+++ ExerciciosVariaveis/src/exerciciosvariaveis/ControleDeFluxo4.java
@@ -13,12 +13,30 @@ public class ControleDeFluxo4 {
 
     public static void main(String[] args) {
         int contador = 0;
+        
+       
+        
+// O WHILE EXECUTA SOMENTE SE FOR TRUE
         while (contador < 10) {
             System.out.println(contador);
             contador++;
-           // contador--;
+
         }
+       
         
+
+// O DO WHILE EXECUTA SENDO TRUE OU FALSE
+        do {
+            System.out.println("AQUI É O VALOR DE DO");
+
+        } while (contador < 10);
+        
+        
+        
+          // O FOR PODE SE CRIAR UMA VARIAVEL LOCAL E VALIDAR
+        for (int i = 0; i < 10; i++) {
+            System.out.println("O valor de i é " + i);
+        }
 
     }
 }
