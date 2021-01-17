4d0d54b2dac08be2fade24023fc8f33e8ed9d110
Willian Sostenes <sostenes655@gmail.com>
2021-01-17 03:33:42 -0300

    Realizando correções no switch

--- ExerciciosVariaveis/src/exerciciosvariaveis/ExercicioControleDeFluxo2.java
+++ ExerciciosVariaveis/src/exerciciosvariaveis/ExercicioControleDeFluxo2.java
@@ -1,41 +1,25 @@
-
- 
 package exerciciosvariaveis;
 
-
 public class ExercicioControleDeFluxo2 {
+
     public static void main(String[] args) {
-        byte dia = 2;
-        switch (dia){
-            case 1 :
-                System.out.println("Domingo");
-                System.out.println("Fim de semana");
+        byte dia = 3;
+        switch (dia) {
+            case 1:
+            case 7:
+                System.out.println("fim de semana");
                 break;
-            case 2 :
-                System.out.println("Segunda");
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
                 System.out.println("Dia útil");
                 break;
-            case 3 :
-                System.out.println("Terça");
-                System.out.println("Dia útil");
-                break;
-            case 4 :
-                System.out.println("Quarta");
-                System.out.println("Dia útil");
-                break;
-            case 5 : 
-                System.out.println("Quinta");
-                System.out.println("Dia útil");
-                break;
-            case 6 :
-                System.out.println("Sexta");
-                System.out.println("Dia útil");
-                break;
-            case 7 :
-                System.out.println("Sabádo");
-                System.out.println("Fim de semana");
-                      
+            default:
+                System.out.println("Opção Inválida");
         }
+
     }
-    
+
 }
