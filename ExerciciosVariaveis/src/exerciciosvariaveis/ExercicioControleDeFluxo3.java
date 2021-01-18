517948323de681cbccfac501a5aac5731fd953fe
Willian Sostenes <sostenes655@gmail.com>
2021-01-18 02:24:53 -0300

    executando exercicios em FOR números pares e impares

--- ExerciciosVariaveis/src/exerciciosvariaveis/ExercicioControleDeFluxo3.java
+++ ExerciciosVariaveis/src/exerciciosvariaveis/ExercicioControleDeFluxo3.java
@@ -10,5 +10,15 @@ package exerciciosvariaveis;
  * @author Forceone
  */
 public class ExercicioControleDeFluxo3 {
-    
+
+    public static void main(String[] args) {
+        int valor = 100;
+        for (int i = 0; i <= valor; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
 }
