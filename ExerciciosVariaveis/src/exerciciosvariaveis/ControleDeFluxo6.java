c7dab6fc1a1a9ee53d8faecf3118100743807717
Willian Sostenes <sostenes655@gmail.com>
2021-01-18 02:57:11 -0300

    Realizando outro exercicio com FOR

---
+++ ExerciciosVariaveis/src/exerciciosvariaveis/ControleDeFluxo6.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosvariaveis;
+
+/**
+ *
+ * @author Forceone
+ */
+public class ControleDeFluxo6 {
+
+    public static void main(String[] args) {
+        double valorCarro = 30000;
+        for (int parcela = (int) valorCarro; parcela >= 1; parcela--) {
+            double valorParcela = valorCarro / parcela;
+            if (valorParcela <= 1000) {
+                continue;
+                System.out.println("teste");
+
+            }
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+
+        }
+    }
+}
