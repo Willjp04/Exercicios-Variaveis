695fb2f3a3473c7d1f40b0d59286040a5a84c3bf
Willian Sostenes <sostenes655@gmail.com>
2021-01-18 05:12:45 -0300

    Realizando exercicios com variaveis e porcentagem

---
+++ ExerciciosVariaveis/src/exerciciosvariaveis/ExercicioCustoDeVeiculo.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosvariaveis;
+
+/**
+ *
+ * @author Forceone
+ */
+public class ExercicioCustoDeVeiculo {
+    public static void main(String[] args) {
+        //CUSTO DE FÁBRICA É CUSTO DIST + CUSTO IMPOSTO.
+        //CUSTO DO DISTIBUIDOR 28%
+        // CUSTO DE IMPOSTOS 45%
+        
+        int custoFabrica = 30000;
+        double custoDist = 0.28;
+        double custoImpo = 0.45;
+        double soma1;
+        double totalComDist;
+        double soma2;
+        double total;
+        
+        double valorFinal;
+        
+        System.out.println("O custo de fábrica do carro é " + "R$ " + custoFabrica + " Reais ");
+        
+        
+        soma1 = custoFabrica * custoDist;
+        
+        soma2 = custoFabrica * custoImpo;
+        
+        total = soma1+soma2;
+        
+        valorFinal = total;
+        
+       
+        
+        
+        
+        System.out.println("O valor Final para o consumidor é " + "R$ " + valorFinal + " Reais");
+        
+        
+        
+        
+        
+        
+    }
+    
+}
