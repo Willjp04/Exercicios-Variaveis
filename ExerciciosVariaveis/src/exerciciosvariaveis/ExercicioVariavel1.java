1ddf3a05efd29403f7910064f50ff5fa032ff140
Willian Sostenes <sostenes655@gmail.com>
2021-01-18 03:56:07 -0300

    Exercitando com variáveis

---
+++ ExerciciosVariaveis/src/exerciciosvariaveis/ExercicioVariavel1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosvariaveis;
+
+/**
+ *
+ * @author Forceone
+ */
+public class ExercicioVariavel1 {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+        
+        a = 20;
+        b = 10;
+        
+        System.out.println("O Valor da varíavel A é " + a);
+        System.out.println("O Valor da varíavel B é " + b);
+    }
+    
+}
