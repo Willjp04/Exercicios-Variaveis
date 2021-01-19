1faecaa70aba715a595fd0a67f94c88f21a2532d
Willian Sostenes <sostenes655@gmail.com>
2021-01-19 19:35:22 -0300

    Continuação exercicios de fixação

---
+++ ExerciciosVariaveis/src/exerciciosvariaveis/ExercicioFixacaoArrays.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosvariaveis;
+
+/**
+ *
+ * @author Forceone
+ */
+public class ExercicioFixacaoArrays {
+
+    public static void main(String[] args) {
+        String[] nomes = new String[3];
+        nomes[0] = "Helloisa";
+        nomes[1] = "Joyce";
+        nomes[2] = "Willian";
+        
+        for(String lista : nomes){
+            System.out.println(lista);
+        }
+
+    }
+
+}
