8f4e1c35cc40ade4c4810f385a57883c5a492839
Willian Sostenes <sostenes655@gmail.com>
2021-01-19 19:12:54 -0300

    Realizando exercicio de fixação para arrays multidimensional

--- ExerciciosVariaveis/src/exerciciosvariaveis/Arrays5.java
+++ ExerciciosVariaveis/src/exerciciosvariaveis/Arrays5.java
@@ -1,21 +1,20 @@
-
 package exerciciosvariaveis;
 
-
 public class Arrays5 {
+
     public static void main(String[] args) {
-        int [][] dias = new int [2][2];
-        dias[0][1] = 30;
+        int[][] dias = new int[2][2];
+        dias[0][0] = 30;
         dias[0][1] = 31;
-        
+
         dias[1][0] = 29;
         dias[1][1] = 28;
-        for(int[] ref: dias ){
-            for(int dia : ref){
+        for (int[] ref : dias) {
+            for (int dia : ref) {
                 System.out.println(dia);
             }
-            
+
         }
-        
+
     }
 }
