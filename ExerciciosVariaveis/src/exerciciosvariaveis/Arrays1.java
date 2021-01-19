15e79036091256832d470fe71ac3be62f2587a4e
Willian Sostenes <sostenes655@gmail.com>
2021-01-18 21:26:04 -0300

    Criando e utilizando arrays

---
+++ ExerciciosVariaveis/src/exerciciosvariaveis/Arrays1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosvariaveis;
+
+/**
+ *
+ * @author Forceone
+ */
+public class Arrays1 {
+
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 6;
+        idades[1] = 25;
+        idades[2] = 28;
+         System.out.println("Idade 1 : " + idades[0]);
+         System.out.println("Idade 2 : " + idades[1]);
+         System.out.println("Idade 3 : " + idades[2]);
+        
+        
+        
+    }
+
+}
