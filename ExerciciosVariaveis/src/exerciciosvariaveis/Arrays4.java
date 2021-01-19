65d65a9fdec2c7546166adb0c0808e4802621d5d
Willian Sostenes <sostenes655@gmail.com>
2021-01-18 22:41:31 -0300

    Exercitando com Arrays parte 4

---
+++ ExerciciosVariaveis/src/exerciciosvariaveis/Arrays4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosvariaveis;
+
+/**
+ *
+ * @author Forceone
+ */
+public class Arrays4 {
+
+    public static void main(String[] args) {
+        String[] nomes = {"Helloisa", "Joyce", "Willian"};
+        for (String listaNomes : nomes) {
+            System.out.println(listaNomes);
+        }
+    }
+
+}
