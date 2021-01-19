eb38f97b9b706c8e3f8b2b2c5f609d70a1a20279
Willian Sostenes <sostenes655@gmail.com>
2021-01-18 22:27:40 -0300

    Exercitando com arrays

---
+++ ExerciciosVariaveis/src/exerciciosvariaveis/Arrays3.java
@@ -0,0 +1,22 @@
+
+package exerciciosvariaveis;
+
+
+public class Arrays3 {
+    public static void main(String[] args) {
+        //FORMULAS DE SE INICIAR UM ARRAY.
+        int[] numeros = new int[5];
+        numeros[0]=1;
+        numeros[1]=50;
+        int[] numeros1 = {1,2,3,4,5}; // INDEX = 0 ATÉ 4.
+        int[] numeros2 =  new int []{1,2,3,4,5};
+//        for(int i = 0;  i<numeros2.length; i++){
+//        System.out.println(numeros2[i]);
+        
+        for(int aux : numeros){
+            System.out.println(aux);
+        }
+        
+    }
+    
+}
