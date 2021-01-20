6ae8725e67715313aad3a4b6513a199e8d53f35d
Willian Sostenes <sostenes655@gmail.com>
2021-01-20 02:31:37 -0300

    Utilizando this no metodo

--- ExerciciosVariaveis/src/Javacore/introducaoclasses/Classes/Carro.java
+++ ExerciciosVariaveis/src/Javacore/introducaoclasses/Classes/Carro.java
@@ -7,10 +7,23 @@ public class Carro {
     public String marca;
     public String cor;
     public float velocidadeMaxima;
+    
+    public void imprime(){
+        System.out.println("-------------");
+        System.out.println(this.placa);
+        System.out.println(this.modelo);
+        System.out.println(this.marca);
+        System.out.println(this.cor);
+        System.out.println(this.velocidadeMaxima);
+    }
+
+
+
+
 //MÉTODOS COM PARAMETROS
 
     public void dadosDoCarro(String placa, String modelo, String marca, String cor, int velocidadeMax) {
-        System.out.println(placa + modelo + marca + velocidadeMax);
+        System.out.println("PLACA DO  " + placa + " MODELO " + modelo + " MARCA " + marca + " VELOCIDADE MAX " + velocidadeMax);
     }
 
 }
--- ExerciciosVariaveis/src/Javacore/introducaoclasses/Classes/test/CarroTest.java
+++ ExerciciosVariaveis/src/Javacore/introducaoclasses/Classes/test/CarroTest.java
@@ -4,24 +4,29 @@
  * and open the template in the editor.
  */
 package Javacore.introducaoclasses.Classes.test;
+
 import Javacore.introducaoclasses.Classes.Carro;
-
-
-
-        
 
 /**
  *
  * @author Forceone
  */
 public class CarroTest {
+
     public static void main(String[] args) {
         Carro carro = new Carro();
-      /*   carro.placa = "ABC-123";
+         carro.placa = "ABC-123";
          carro.modelo = "CORSA WIND";
          carro.marca = "Chevrolet";
          carro.cor = "Azul";
          carro.velocidadeMaxima = 140;
+         
+         Carro carro2 = new Carro();
+         carro2.placa = "AAA-3333";
+         carro2.modelo = "FIESTA";
+         carro2.marca = "FORD";
+         carro2.cor = "PRETO";
+         carro2.velocidadeMaxima = 160;
          
          System.out.println(" ESPECIFICAÇÃO VEICULO ");
            
@@ -29,20 +34,25 @@ public class CarroTest {
          System.out.println("///////////////////");
          
          
-         System.out.println(carro.placa);
-         System.out.println(carro.modelo);
-         System.out.println(carro.marca);
-         System.out.println(carro.cor);
-         System.out.println(carro.velocidadeMaxima + " Km");
+        carro.imprime();
+        
+        
+        System.out.println(" ESPECIFICAÇÃO VEICULO ");
          
-         */
-     // EXERCITANDO MÉTODOS COM PARAMETROS E ARGUMENTOS.
-      carro.dadosDoCarro("CDB-7972 ", "CORSA ", "Chevrolet ", "Azul ", 160 );
-      
+         carro2.imprime();
+       
+        
+        /* EXERCITANDO MÉTODOS COM PARAMETROS E ARGUMENTOS.
+        carro.dadosDoCarro("CDB-7972 ", "CORSA ", "CHEVROLET ", "Azul ", 160);
+
         System.out.println("--------------------");
-      carro.dadosDoCarro("AAA-5558", " UNO ", "FIAT ", "BRANCO ", 170);
-      System.out.println("--------------------");
-      carro.dadosDoCarro("BBB-8885 ", "FIESTA ", "FORD ", " PRETO ", 180);
+        carro.dadosDoCarro("AAA-5558", " UNO ", "FIAT ", "BRANCO ", 170);
+        System.out.println("--------------------");
+        carro.dadosDoCarro("BBB-8885 ", "FIESTA ", "FORD ", " PRETO ", 180);
+        System.out.println("--------------------");
+
+        carro.dadosDoCarro("CCC-1111 ", "GOL ", "Volkswagen ", "CINZA ", 160);*/
     }
     
+
 }
